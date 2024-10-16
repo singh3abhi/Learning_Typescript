63c28f4554ffd41d74278d0f1a7752089c371513
singh3abhi <singh3abhi@gmail.com>
2024-10-17 02:30:47 +0530

    Completed Typescript Folder Structure

---
+++ Folder_Structure/src/index.ts
@@ -0,0 +1,17 @@
+
+/*
+
+We will generate ts config file
+tsc -init
+
+After confguring the root and outDir then we will start the watcher
+
+tsc -w
+
+This folder structure is rarely used as the typescript is used with react or node js and they will have
+their own respective folder structures
+*/
+
+const a = 26;
+alert(a);
+console.log(a);
